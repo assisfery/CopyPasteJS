5b5eaab0268d4b6f919f3c805eaf8a24e5515bb3
Assis Ferreira <ferreira.assisruny@gmail.com>
2020-02-16 22:15:28 -0100

    Correction Node Module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
  
 // Read and eval library
-filedata = fs.readFileSync('./CopyPasteJS.js','utf8');
+filedata = fs.readFileSync('src/CopyPasteJS.js','utf8');
 eval(filedata);
  
 exports = CopyPasteJS;
